9795339828c1069575b2bb9c4b670b05367b1464
egor4ka <serbakove400@gmail.com>
2022-02-02 15:55:47 +0000

    Changed == to =

--- powerbox.sh
+++ powerbox.sh
@@ -1,24 +1,29 @@
 mount -o rw,remount /
 echo "Which directory would you like to install powerbox to? (/linux is recommended): "
 read -r powerdir
+
 if [ "$powerdir" == "" ]
 then
   powerdir = /linux
 fi
+
 mkdir $powerdir
 echo "How many gigabytes of space would you like to allocate to the chroot image? (no symbols, in gigabytes): "
+
 read -r powersize 
 if [ "$powersize" == "" ]
 then
-  powersize == 10
+  powersize = 10
 fi
 powerimgsize = powersize * 1000000 / 16
 echo "How would you like to name the image? (linux is recommended): "
 read -r powerimgname
+
 if [ "$powerimgname" == "" ]
 then
   powerimgname = "linux"
 fi
+
 echo "Creating image..."
 dd if=/dev/zero of=/sdcard/$powerimgname.img bs=16k count=$powerimgsize
 mkfs.ext4 /sdcard/$powerimgname
