396e65be4988123558abdf7023459ad72ba269f4
egor4ka <serbakove400@gmail.com>
2022-02-02 17:49:53 +0000

    hello

--- powerbox-build.sh
+++ powerbox-build.sh
@@ -1,8 +1,8 @@
 wget https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/aarch64/alpine-minirootfs-3.15.0-aarch64.tar.gz
-mkdir .j/alpine
+mkdir ./alpine
 tar -xvf alpine-minirootfs-3.15.0-aarch64.tar.gz -C ./alpine/
 chroot ./alpine /bin/sh<<"EOT"
-apk add bat github-cli git alpine-base doas zsh shadow bash openssh gcc alpine-sdk
+apk add bat curl wget github-cli git alpine-base doas zsh shadow bash openssh gcc alpine-sdk
 source /etc/profile
 useradd powerbox
 groupadd -g 3003 inet
@@ -16,3 +16,6 @@ mkdir /home/powerbox/git
 mkdir /home/powerbox/downloads
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/powerbox/git/powerlevel10k
 echo 'source /home/powerbox/git/powerlevel10k/powerlevel10k.zsh-theme' >>/home/powerbox/.zshrc
+wget https://raw.githubusercontent.com/egor4ka/powerbox/main/profile
+mv ./profile /etc/profile
+EOT
