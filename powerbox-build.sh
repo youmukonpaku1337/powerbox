d68d99f9944cd7ebda4b98184637222d7f6d7b82
egor4ka <serbakove400@gmail.com>
2022-02-03 06:48:35 +0000

    hello

--- powerbox-build.sh
+++ powerbox-build.sh
@@ -1,17 +1,15 @@
-hostarch = $(uname -m)
-rm ./powerbox-$hostarch.tar.gz
+HOSTARCH=$(uname -m)
+rm ./powerbox-$HOSTARCH.tar.gz
 mkdir ./alpine
 
-curl -LO https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/$hostarch/alpine-minirootfs-3.15.0-$hostarch.tar.gz
-tar -xvf alpine-minirootfs-3.15.0-$hostarch.tar.gz -C ./alpine/
+curl -LO https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/$HOSTARCH/alpine-minirootfs-3.15.0-$HOSTARCH.tar.gz
+tar -xvf alpine-minirootfs-3.15.0-$HOSTARCH.tar.gz -C ./alpine/
 
 doas chroot ./alpine /bin/sh<<"EOT"
 source /etc/profile
-useradd powerbox
-groupadd -g 3003 inet
-usermod -a -G inet --shell /bin/zsh root
-usermod -a -G inet --shell /bin/zsh powerbox
-su root
+addgroup -g 3003 -S inet
+addgroup root inet
+adduser -s /bin/zsh -g inet powerbox
 rm /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apk add bat curl wget nim github-cli git alpine-base doas zsh shadow bash openssh gcc alpine-sdk
@@ -19,6 +17,7 @@ mkdir -p /home/powerbox/git
 mkdir /home/powerbox/downloads
 mkdir -p /run/openrc/
 mkdir -p /lib64/rc/init.d/
+ln -s /usr/bin/doas /usr/bin/sudo
 ln -s /lib64/rc/init.d /run/openrc
 touch /run/openrc/softlevel
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/powerbox/git/powerlevel10k
@@ -27,6 +26,7 @@ EOT
 
 doas /bin/sh<<"EOT"
 cp ./profile ./alpine/etc/
-tar -zcvf ./powerbox.tar.gz ./alpine
+tar -zcvf ./powerbox-$HOSTARCH.tar.gz ./alpine
 rm -rf ./alpine
+rm alpine-minirootfs-3.15.0-$HOSTARCH.tar.gz
 EOT
